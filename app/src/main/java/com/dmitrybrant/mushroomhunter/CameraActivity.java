f729f84bd6672e0c7a94fba03d67514a311aea59
Dmitry Brant <me@dmitrybrant.com>
2017-07-15 11:26:38 -0400

    Initial commit.

---
+++ app/src/main/java/com/dmitrybrant/mushroomhunter/CameraActivity.java
@@ -0,0 +1,204 @@
+/*
+ * Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.dmitrybrant.mushroomhunter;
+
+import android.Manifest;
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.pm.PackageManager;
+import android.media.Image.Plane;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.util.Log;
+import android.util.Size;
+import android.view.KeyEvent;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import java.nio.ByteBuffer;
+
+public abstract class CameraActivity extends Activity implements OnImageAvailableListener {
+    private static final String TAG = "CameraActivity";
+    private static final int PERMISSIONS_REQUEST = 1;
+
+    private static final String PERMISSION_CAMERA = Manifest.permission.CAMERA;
+    private static final String PERMISSION_STORAGE = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
+    private boolean debug = false;
+
+    private Handler handler;
+    private HandlerThread handlerThread;
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(null);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.activity_camera);
+
+        if (hasPermission()) {
+            setFragment();
+        } else {
+            requestPermission();
+        }
+    }
+
+    @Override
+    public synchronized void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    public synchronized void onResume() {
+        super.onResume();
+
+        handlerThread = new HandlerThread("inference");
+        handlerThread.start();
+        handler = new Handler(handlerThread.getLooper());
+    }
+
+    @Override
+    public synchronized void onPause() {
+
+        if (!isFinishing()) {
+            finish();
+        }
+
+        handlerThread.quitSafely();
+        try {
+            handlerThread.join();
+            handlerThread = null;
+            handler = null;
+        } catch (final InterruptedException e) {
+            Log.w(TAG, "Exception!");
+        }
+
+        super.onPause();
+    }
+
+    protected synchronized void runInBackground(final Runnable r) {
+        if (handler != null) {
+            handler.post(r);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            final int requestCode, final String[] permissions, final int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST: {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    setFragment();
+                } else {
+                    requestPermission();
+                }
+            }
+        }
+    }
+
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(PERMISSION_CAMERA) == PackageManager.PERMISSION_GRANTED && checkSelfPermission(PERMISSION_STORAGE) == PackageManager.PERMISSION_GRANTED;
+        } else {
+            return true;
+        }
+    }
+
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (shouldShowRequestPermissionRationale(PERMISSION_CAMERA) || shouldShowRequestPermissionRationale(PERMISSION_STORAGE)) {
+                Toast.makeText(CameraActivity.this, "Camera AND storage permission are required for this demo", Toast.LENGTH_LONG).show();
+            }
+            requestPermissions(new String[]{PERMISSION_CAMERA, PERMISSION_STORAGE}, PERMISSIONS_REQUEST);
+        }
+    }
+
+    protected void setFragment() {
+        final Fragment fragment =
+                CameraConnectionFragment.newInstance(
+                        new CameraConnectionFragment.ConnectionCallback() {
+                            @Override
+                            public void onPreviewSizeChosen(final Size size, final int rotation) {
+                                CameraActivity.this.onPreviewSizeChosen(size, rotation);
+                            }
+                        },
+                        this,
+                        getLayoutId(),
+                        getDesiredPreviewFrameSize());
+
+        getFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .commit();
+    }
+
+    protected void fillBytes(final Plane[] planes, final byte[][] yuvBytes) {
+        // Because of the variable row stride it's not possible to know in
+        // advance the actual necessary dimensions of the yuv planes.
+        for (int i = 0; i < planes.length; ++i) {
+            final ByteBuffer buffer = planes[i].getBuffer();
+            if (yuvBytes[i] == null) {
+                Log.d(TAG, "Initializing buffer " + i + " at size " + buffer.capacity());
+                yuvBytes[i] = new byte[buffer.capacity()];
+            }
+            buffer.get(yuvBytes[i]);
+        }
+    }
+
+    public boolean isDebug() {
+        return debug;
+    }
+
+    public void requestRender() {
+        final OverlayView overlay = (OverlayView) findViewById(R.id.debug_overlay);
+        if (overlay != null) {
+            overlay.postInvalidate();
+        }
+    }
+
+    public void addCallback(final OverlayView.DrawCallback callback) {
+        final OverlayView overlay = (OverlayView) findViewById(R.id.debug_overlay);
+        if (overlay != null) {
+            overlay.addCallback(callback);
+        }
+    }
+
+    public void onSetDebug(final boolean debug) {
+    }
+
+    @Override
+    public boolean onKeyDown(final int keyCode, final KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN || keyCode == KeyEvent.KEYCODE_VOLUME_UP) {
+            debug = !debug;
+            requestRender();
+            onSetDebug(debug);
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    protected abstract void onPreviewSizeChosen(final Size size, final int rotation);
+
+    protected abstract int getLayoutId();
+
+    protected abstract Size getDesiredPreviewFrameSize();
+}
---
+++ app/src/main/java/com/dmitrybrant/mushroomhunter/ClassifierActivity.java
@@ -0,0 +1,292 @@
+package com.dmitrybrant.mushroomhunter;
+
+import android.graphics.Bitmap;
+import android.graphics.Bitmap.Config;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.media.Image;
+import android.media.Image.Plane;
+import android.media.ImageReader;
+import android.media.ImageReader.OnImageAvailableListener;
+import android.os.SystemClock;
+import android.os.Trace;
+import android.util.Size;
+import android.view.Display;
+import android.widget.TextView;
+
+import org.tensorflow.demo.env.ImageUtils;
+
+import java.util.List;
+
+public class ClassifierActivity extends CameraActivity implements OnImageAvailableListener {
+
+    // These are the settings for the original v1 Inception model. If you want to
+    // use a model that's been produced from the TensorFlow for Poets codelab,
+    // you'll need to set IMAGE_SIZE = 299, IMAGE_MEAN = 128, IMAGE_STD = 128,
+    // INPUT_NAME = "Mul", and OUTPUT_NAME = "final_result".
+    // You'll also need to update the MODEL_FILE and LABEL_FILE paths to point to
+    // the ones you produced.
+    //
+    // To use v3 Inception model, strip the DecodeJpeg Op from your retrained
+    // model first:
+    //
+    // python strip_unused.py \
+    // --input_graph=<retrained-pb-file> \
+    // --output_graph=<your-stripped-pb-file> \
+    // --input_node_names="Mul" \
+    // --output_node_names="final_result" \
+    // --input_binary=true
+
+    /*
+    private static final int INPUT_SIZE = 224;
+    private static final int IMAGE_MEAN = 117;
+    private static final float IMAGE_STD = 1;
+    private static final String INPUT_NAME = "input";
+    private static final String OUTPUT_NAME = "output";
+    */
+
+    /*
+    // retrained Inception V3
+    private static final int INPUT_SIZE = 299;
+    private static final int IMAGE_MEAN = 128;
+    private static final float IMAGE_STD = 128;
+    private static final String INPUT_NAME = "Mul";
+    private static final String OUTPUT_NAME = "final_result";
+    */
+
+    // retrained MobileNet v1
+    private static final int INPUT_SIZE = 224;
+    private static final int IMAGE_MEAN = 128;
+    private static final float IMAGE_STD = 128;
+    private static final String INPUT_NAME = "input";
+    private static final String OUTPUT_NAME = "final_result";
+
+
+
+    private static final String MODEL_FILE = "file:///android_asset/mushrooms.pb"; //tensorflow_inception_graph.pb";
+
+    private static final String LABEL_FILE = "file:///android_asset/output_labels.txt";  //imagenet_comp_graph_label_strings.txt";
+
+    private static final boolean SAVE_PREVIEW_BITMAP = false;
+
+    private static final boolean MAINTAIN_ASPECT = true;
+
+    private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
+
+    private Classifier classifier;
+
+    private Integer sensorOrientation;
+
+    private int previewWidth = 0;
+    private int previewHeight = 0;
+    private byte[][] yuvBytes;
+    private int[] rgbBytes = null;
+    private Bitmap rgbFrameBitmap = null;
+    private Bitmap croppedBitmap = null;
+
+    private Bitmap cropCopyBitmap;
+
+    private boolean computing = false;
+
+    private Matrix frameToCropTransform;
+    private Matrix cropToFrameTransform;
+
+    private TextView resultsView;
+
+    private TextView borderedText;
+
+    private long lastProcessingTimeMs;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.camera_connection_fragment;
+    }
+
+    @Override
+    protected Size getDesiredPreviewFrameSize() {
+        return DESIRED_PREVIEW_SIZE;
+    }
+
+    @Override
+    public void onPreviewSizeChosen(final Size size, final int rotation) {
+        borderedText = new TextView(this);
+        borderedText.setTypeface(Typeface.MONOSPACE);
+
+        classifier =
+                TensorFlowImageClassifier.create(
+                        getAssets(),
+                        MODEL_FILE,
+                        LABEL_FILE,
+                        INPUT_SIZE,
+                        IMAGE_MEAN,
+                        IMAGE_STD,
+                        INPUT_NAME,
+                        OUTPUT_NAME);
+
+        resultsView = (TextView) findViewById(R.id.results);
+        previewWidth = size.getWidth();
+        previewHeight = size.getHeight();
+
+        final Display display = getWindowManager().getDefaultDisplay();
+        final int screenOrientation = display.getRotation();
+
+        //LOGGER.i("Sensor orientation: %d, Screen orientation: %d", rotation, screenOrientation);
+
+        sensorOrientation = rotation + screenOrientation;
+
+        //LOGGER.i("Initializing at size %dx%d", previewWidth, previewHeight);
+        rgbBytes = new int[previewWidth * previewHeight];
+        rgbFrameBitmap = Bitmap.createBitmap(previewWidth, previewHeight, Config.ARGB_8888);
+        croppedBitmap = Bitmap.createBitmap(INPUT_SIZE, INPUT_SIZE, Config.ARGB_8888);
+
+        frameToCropTransform =
+                ImageUtils.getTransformationMatrix(
+                        previewWidth, previewHeight,
+                        INPUT_SIZE, INPUT_SIZE,
+                        sensorOrientation, MAINTAIN_ASPECT);
+
+        cropToFrameTransform = new Matrix();
+        frameToCropTransform.invert(cropToFrameTransform);
+
+        yuvBytes = new byte[3][];
+
+        addCallback(
+                new OverlayView.DrawCallback() {
+                    @Override
+                    public void drawCallback(final Canvas canvas) {
+                        renderDebug(canvas);
+                    }
+                });
+    }
+
+    @Override
+    public void onImageAvailable(final ImageReader reader) {
+        Image image = null;
+
+        try {
+            image = reader.acquireLatestImage();
+
+            if (image == null) {
+                return;
+            }
+
+            if (computing) {
+                image.close();
+                return;
+            }
+            computing = true;
+
+            Trace.beginSection("imageAvailable");
+
+            final Plane[] planes = image.getPlanes();
+            fillBytes(planes, yuvBytes);
+
+            final int yRowStride = planes[0].getRowStride();
+            final int uvRowStride = planes[1].getRowStride();
+            final int uvPixelStride = planes[1].getPixelStride();
+            ImageUtils.convertYUV420ToARGB8888(
+                    yuvBytes[0],
+                    yuvBytes[1],
+                    yuvBytes[2],
+                    previewWidth,
+                    previewHeight,
+                    yRowStride,
+                    uvRowStride,
+                    uvPixelStride,
+                    rgbBytes);
+
+            image.close();
+        } catch (final Exception e) {
+            if (image != null) {
+                image.close();
+            }
+            //LOGGER.e(e, "Exception!");
+            Trace.endSection();
+            return;
+        }
+
+        rgbFrameBitmap.setPixels(rgbBytes, 0, previewWidth, 0, 0, previewWidth, previewHeight);
+        final Canvas canvas = new Canvas(croppedBitmap);
+        canvas.drawBitmap(rgbFrameBitmap, frameToCropTransform, null);
+
+        // For examining the actual TF input.
+        if (SAVE_PREVIEW_BITMAP) {
+            ImageUtils.saveBitmap(croppedBitmap);
+        }
+
+        runInBackground(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        final long startTime = SystemClock.uptimeMillis();
+                        final List<Classifier.Recognition> results = classifier.recognizeImage(croppedBitmap);
+                        lastProcessingTimeMs = SystemClock.uptimeMillis() - startTime;
+
+                        displayResults(results);
+                        cropCopyBitmap = Bitmap.createBitmap(croppedBitmap);
+
+                        requestRender();
+                        computing = false;
+                    }
+                });
+
+        Trace.endSection();
+    }
+
+    @Override
+    public void onSetDebug(boolean debug) {
+        classifier.enableStatLogging(debug);
+    }
+
+    private void displayResults(List<Classifier.Recognition> results) {
+        final StringBuilder sb = new StringBuilder();
+        for (Classifier.Recognition rec : results) {
+            sb.append(rec.getTitle());
+            sb.append(" (");
+            sb.append((int)(rec.getConfidence() * 100f));
+            sb.append("%)\n");
+        }
+        resultsView.post(new Runnable() {
+            @Override
+            public void run() {
+                resultsView.setText(sb.toString());
+            }
+        });
+    }
+
+    private void renderDebug(final Canvas canvas) {
+        if (!isDebug()) {
+            return;
+        }
+        final Bitmap copy = cropCopyBitmap;
+        if (copy != null) {
+            final Matrix matrix = new Matrix();
+            final float scaleFactor = 2;
+            matrix.postScale(scaleFactor, scaleFactor);
+            matrix.postTranslate(
+                    canvas.getWidth() - copy.getWidth() * scaleFactor,
+                    canvas.getHeight() - copy.getHeight() * scaleFactor);
+            canvas.drawBitmap(copy, matrix, new Paint());
+
+            StringBuilder lines = new StringBuilder();
+            if (classifier != null) {
+                String statString = classifier.getStatString();
+                String[] statLines = statString.split("\n");
+                for (String line : statLines) {
+                    lines.append(line);
+                    lines.append("\n");
+                }
+            }
+
+            lines.append("Frame: " + previewWidth + "x" + previewHeight);
+            lines.append("Crop: " + copy.getWidth() + "x" + copy.getHeight());
+            lines.append("View: " + canvas.getWidth() + "x" + canvas.getHeight());
+            lines.append("Rotation: " + sensorOrientation);
+            lines.append("Inference time: " + lastProcessingTimeMs + "ms");
+
+            borderedText.setText(lines);
+        }
+    }
+}
